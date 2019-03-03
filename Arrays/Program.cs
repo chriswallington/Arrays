5e37a633a86612976d218627f1160a19ccf151ba
Chris Wallington <chris@carpedatum.co.uk>
2019-03-03 14:31:05 +0000

    added foreach statement instead of for

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,19 +10,31 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            var numberDictionary = new Dictionary<int, string>();
-            {
-                int[] randomNumbers = { 1, 1, 3, 5, 4, 2, 6, 4, 2, 7, 74, 7, 8, 4, 7, 2, 5, 4, 6, 1 };
-                for (int i = 0; i < randomNumbers.Length; i++)
-                {
-                    if (!randomNumbers.Contains(i))
-                    {
-                        numberDictionary.Add(i, "random number");
-                    }
-                    
-                }
+            int[] randomNumbers = { 1, 1, 3, 5, 4, 2, 6, 4, 2, 7, 74, 7, 8, 4, 7, 2, 5, 4, 6, 1 };
 
-            }
+            var numberDictionary = new Dictionary<int, int>();
+           
+            foreach (var number in randomNumbers)
+             {
+                 if (!numberDictionary.ContainsValue(number))
+                 {
+                    numberDictionary.Add(number, number);
+                 }
+             }
+            
+
+
+                //for (int i = 0; i < randomNumbers.Length; i++)
+                //{
+                //    if (!randomNumbers.Contains(i))
+                //    {
+                //        numberDictionary.Add(randomNumbers[i],"non duplicate number");
+                        
+                //    }
+                    
+                //}
+
+            
             
 
 
