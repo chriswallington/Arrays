a5a799b46db922a1d595d50317c89cf6145dd563
Chris Wallington <chris@carpedatum.co.uk>
2019-03-04 13:41:52 +0000

    Using a sorted dictionary

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,40 +6,50 @@ using System.Threading.Tasks;
 
 namespace Arrays
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
             int[] randomNumbers = { 1, 1, 3, 5, 4, 2, 6, 4, 2, 7, 74, 7, 8, 4, 7, 2, 5, 4, 6, 1 };
 
-            var numberDictionary = new Dictionary<int, int>();
-           
-            foreach (var number in randomNumbers)
-             {
-                 if (!numberDictionary.ContainsValue(number))
-                 {
-                    numberDictionary.Add(number, number);
-                 }
-             }
+            var numberDictionary = new SortedDictionary<int, int>();
+
+            DeDuplicateDictionary(randomNumbers, numberDictionary);
+
             
 
-
-                //for (int i = 0; i < randomNumbers.Length; i++)
-                //{
-                //    if (!randomNumbers.Contains(i))
-                //    {
-                //        numberDictionary.Add(randomNumbers[i],"non duplicate number");
+            //numberDictionary.OrderBy(x => x.Value).ToList();
+            
+            
                         
-                //    }
-                    
-                //}
 
-            
-            
+
+            //for (int i = 0; i < randomNumbers.Length; i++)
+            //{
+            //    if (!randomNumbers.Contains(i))
+            //    {
+            //        numberDictionary.Add(randomNumbers[i],"non duplicate number");
+
+            //    }
+
+            //}
+
+
+
 
 
 
         }
 
+        private static void DeDuplicateDictionary(int[] randomNumbers, SortedDictionary<int, int> numberDictionary)
+        {
+            foreach (var number in randomNumbers)
+            {
+                if (!numberDictionary.ContainsValue(number))
+                {
+                    numberDictionary.Add(number, number);
+                }
+            }
+        }
     }
 }
